cd1f52f9b7ce0232bb7fba0fd6f8968d69aa3abc
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-01-20 11:36:09 +0100

    add project

---
+++ Models/EmployeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tp2_MVC.Models
+{
+    public class EmployeDbContext : DbContext
+    {
+        public EmployeDbContext(DbContextOptions<EmployeDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace tp2_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseKestrel(o => { o.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(20); })
+            .UseStartup<Startup>().UseUrls("http://127.0.0.1:5000").UseIISIntegration();
+
+    }
+}
